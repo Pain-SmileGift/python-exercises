2ad941cc24c42eab89a4193b49fae548dfff17df
Pain-SmileGift <916037963@qq.com>
2023-02-16 14:42:12 +0800

    Update 挑选礼物.py

--- 挑选礼物.py
+++ 挑选礼物.py
@@ -28,3 +28,21 @@ print(Res[-1])
 '''
 方法二：01背包
 '''
+# 物品重量
+w=[2,2,6,5,4]
+# 物品价值
+v=[6,3,5,4,6]
+# 共5个物品，背包承重7
+N=5
+W=7
+# 生成N*W二维数组
+result=[[0 for j in range(1+W)] for i in range(1+N)]
+
+for i in range(1,N+1):
+    for j in range(1,W+1):
+        if w[i-1]>j:
+            result[i][j]=result[i-1][j]
+        else:
+            result[i][j]=max(result[i-1][j],result[i-1][j-w[i-1]]+v[i-1])
+
+print(result[N][W])
